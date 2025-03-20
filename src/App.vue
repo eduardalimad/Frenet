9b227a6ce118f9ba73459572ba3cb3034b547fff
Eduarda Lima <me.eduuarda@gmail.com>
2025-03-20 12:55:39 -0400

    :sparkles: feat: Instalação do Pinia e criação dos arquivos base

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
+import Load from "@/components/Load.vue";
+import { useLoadingStore } from "./stores/loading";
+
+const loadingStore = useLoadingStore();
 </script>
 
 <template>
   <RouterView />
+  <Load v-if="loadingStore.isLoading"/>
 </template>
 
 <style scoped lang="sccs"></style>
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import App from './App.vue'
 import router from './router'
 import registerComponents from './plugins/prime.ts'
 import KeyFilter from 'primevue/keyfilter';
+import { createPinia } from 'pinia';
 
 const app = createApp(App)
 app.directive('keyfilter', KeyFilter);
 
 app.use(router)
+app.use(createPinia());
 registerComponents(app);
 app.mount('#app')
