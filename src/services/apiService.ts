2228674690e8bf45e95e2ee64cea2c0e3921fdf8
Eduarda Lima <me.eduuarda@gmail.com>
2025-03-24 19:53:19 -0400

    :recycle: refactor: otimização de funções e melhorias gerais no código para aumentar eficiência

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,10 +1,11 @@
+import type { Address, FreightQuotesResponse } from "cep-types";
 import { http } from "../services/index.ts";
 
 export default {
   listProvidingQuotes: async (data: object) => {
-    return await http.post('shipping/quote', data);
+    return await http.post<FreightQuotesResponse>('shipping/quote', data);
   },
   searchCep: async (params: string) => {
-    return await http.get(`/CEP/Address/${params}`);
+    return await http.get<Address>(`/CEP/Address/${params}`);
   },
 };
