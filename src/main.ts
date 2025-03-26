d1b22f27e1846d51768249e8d48e8c0124d47ea8
Eduarda Lima <me.eduuarda@gmail.com>
2025-03-25 21:09:56 -0400

    feat: instala e configura vue-toastification para feedbacks de usuário

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ import router from './router'
 import registerComponents from './plugins/prime.ts'
 import KeyFilter from 'primevue/keyfilter';
 import { createPinia } from 'pinia';
+import Toast, { type PluginOptions } from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
 
 const app = createApp(App)
 app.directive('keyfilter', KeyFilter);
 
 app.use(router)
+app.use(Toast);
 app.use(createPinia());
 registerComponents(app);
 app.mount('#app')
